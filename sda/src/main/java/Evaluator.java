9fe04ada9af40228d08facb41db1ba9fb644247b
dandenisa <denisa.dan@garmin.com>
2020-01-06 16:45:22 +0200

    small changes

--- sda/src/main/java/Evaluator.java
+++
@@ -1,3 +0,0 @@
-public interface Evaluator {
-	public StatusCandidat evalueaza(Candidat candidat);
-}
--- sda/src/main/java/IncapacitateEvaluare.java
+++
@@ -1,5 +0,0 @@
-public class IncapacitateEvaluare extends RuntimeException {
-	public IncapacitateEvaluare(String message) {
-		super(message);
-	}
-}
